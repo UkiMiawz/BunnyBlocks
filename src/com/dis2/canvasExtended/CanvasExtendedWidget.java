83992f11f23f11a76332438c752eef313f9fe443
UkiMiawz <melisa.cecilia.mail@gmail.com>
2016-07-02 16:46:51 +0200

    Rename file

---
+++ src/com/dis2/canvasExtended/CanvasExtendedWidget.java
@@ -0,0 +1,7 @@
+package com.dis2.canvasExtended;
+
+/**
+ * Created by woooowowowo.
+ */
+public class CanvasExtendedWidget {
+}
--- src/com/dis2/canvasExtended/canvasExtendedWidget.java
+++
@@ -1,7 +0,0 @@
-package com.dis2.canvasExtended;
-
-/**
- * Created by ukimiawz on 7/2/16.
- */
-public class canvasExtendedWidget {
-}
