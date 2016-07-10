9e6b79de0f43a5bcf5ebaf236d99b5ec4a6b9d85
UkiMiawz <melisa.cecilia.mail@gmail.com>
2016-07-10 16:49:42 +0200

    Add new class for class compiler

---
+++ src/com/dis2/shared/CardCompiler.java
@@ -0,0 +1,16 @@
+package com.dis2.shared;
+
+import com.dis2.cards2.Card;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Slartirbarfast.
+ */
+public class CardCompiler {
+    public static ArrayList<AnimationAction> compileCards(ArrayList<Card> inputCards){
+        ArrayList<AnimationAction> result = new ArrayList();
+
+        return result;
+    }
+}
