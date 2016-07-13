1032e23f00588512b2df9a607cb6ab9011bc1f23
UkiMiawz <melisa.cecilia.mail@gmail.com>
2016-07-13 10:51:25 +0200

    Add overlay detection

--- src/com/dis2/shared/MovementConstants.java
+++ src/com/dis2/shared/MovementConstants.java
@@ -8,6 +8,9 @@ public class MovementConstants {
     private int xBlockHorizontal = 55;
     private int yBlockHorizontal = -33;
 
+    public int getWidth() {return xBlockVertical;}
+    public int getHeight() {return yBlockVertical;}
+
     public MovementConstants(int xBlockSize, int yBlockSize){
         xBlockVertical = xBlockSize;
         yBlockVertical = yBlockSize;
